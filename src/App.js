db9ec39ab8cc6745a244f8d017dcf9c571b45c9a
iqbalpa <iqbalpahlevi16@gmail.com>
2023-02-02 21:08:00 +0700

    add loading button and change card button color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchBar from "./components/SearchBar/SearchBar";
 
 export default function App() {
 	return (
-		<div className="flex justify-center items-center h-screen bg-sky-400">
+		<div className="flex justify-center items-center h-screen bg-gradient-to-br from-fuchsia-500 to-sky-500">
 			<SearchBar />
 		</div>
 	);
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -40,7 +40,7 @@ const SearchBar = () => {
 	}, [data]);
 
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col justify-center items-center">
 			<form className="w-[34rem] m-6" onSubmit={handleSubmit}>
 				<label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
 					Search
@@ -69,7 +69,19 @@ const SearchBar = () => {
 				</div>
 			</form>
 			<div>
-				{loading && <p className="text-center">Loading...</p>}
+				{loading && (
+					<button
+						type="button"
+						class="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-red-500 hover:bg-red-400 transition ease-in-out duration-150 cursor-not-allowed"
+						disabled=""
+					>
+						<svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+							<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+							<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+						</svg>
+						Loading...
+					</button>
+				)}
 				{error && <p>Something went wrong...</p>}
 				{!loading && data && <Card number={value} data={data} />}
 			</div>
